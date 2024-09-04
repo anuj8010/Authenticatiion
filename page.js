6998e6f079b3ff3a2967676c3680a7a30a09bd8a
anuj8010 <anujjangid899@gmail.com>
2024-09-04 22:32:15 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import businessdata from './busdata'
+import Component from './auth'
+import Businessdata from './busdata'
+const business= () => {
+  return (
+    <div>
+      {/* <Businessdata/> */}
+<Component/>
+    </div>
+  )
+}
+
+export default business
+
